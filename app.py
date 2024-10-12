1f1296af6a7174864db784f22398af6d6df5d033
Tugiyanto <94089340+Tugiyanto@users.noreply.github.com>
2024-10-12 23:34:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ tf.keras.utils.get_custom_objects().update({
 
 # Set working directory and model path
 working_dir = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(working_dir, 'cnn_model.h5')
+model_path = os.path.join(working_dir, 'cnn_model.keras')
 
 # Load the pre-trained model
 model = tf.keras.models.load_model(model_path, custom_objects={'SparseCategoricalCrossentropy': CustomSparseCategoricalCrossentropy})
@@ -29,10 +29,10 @@ class_names = ['normal', 'cracked']
 # Function to preprocess the uploaded image
 def preprocess_image(image):
     img = Image.open(image)
-    img = img.resize((28, 28))
+    img = img.resize((224, 224))
     img = img.convert('L')  # Convert to grayscale
     img_array = np.array(img) / 255.0
-    img_array = img_array.reshape((1, 28, 28, 1))
+    img_array = img_array.reshape((1, 224, 224, 1))
     return img_array
 
 # Streamlit App
